ce68e84bd5a0406eb3a0db239efef5aee935e084
Kunta Kinte <kunta.kinte@dreamnation.net>
2017-02-24 09:59:13 -0800

    create makeit.sh script and prebuildtomake.cs
    ...so can be build without nant or xbuild

---
+++ makeit.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+cd `dirname $0`
+mono --debug prebuildtomake.exe > makefile
+make -f makefile MCS=mcs "$@"
+
+function filternames
+{
+    natives=' openjpeg-dotnet.dll BulletSim.dll ode.dll openjpeg-dotnet-x86_64.dll Npgsql.dll sqlite3.dll '
+    while read name
+    do
+        nameonly=${name##*/}
+        if [ "${natives/ $nameonly /}" != "$natives" ]
+        then
+            continue
+        fi
+        if [ $name.so -ot $name ]
+        then
+            echo $name
+        fi
+    done
+}
+
+function deloldsos
+{
+    while read nameso
+    do
+        name=${nameso%.so}
+        if [ ! -f $name ]
+        then
+            rm $nameso
+            continue
+        fi
+        if [ $nameso -ot $name ]
+        then
+            rm $nameso
+            continue
+        fi
+    done
+}
+
+find bin -name \*.dll | filternames | xargs -n 1 -P 8 -r -t mono --aot -O=all
+find bin -name \*.exe | filternames | xargs -n 1 -P 8 -r -t mono --aot -O=all
+find bin -name \*.dll.so | deloldsos
+find bin -name \*.exe.so | deloldsos
+
